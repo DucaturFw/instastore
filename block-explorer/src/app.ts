e37e42b87df0fa23b83b317ac136d6336682f5ee
Dmitry Radkovskiy (zlumer) <zlumer@gmail.com>
2018-04-07 19:59:14 +0300

    frontend

--- block-explorer/src/app.ts
+++ block-explorer/src/app.ts
@@ -1,6 +1,13 @@
 console.log("hello")
 
 import { app } from "./restapi"
+import { readFileSync } from "fs";
+import * as path from "path";
+
+app.get('/', (req, res) =>
+{
+	res.send(readFileSync(path.join(__dirname, "../static/index.html")).toString('utf8'))
+})
 
 const PORT = 8814
 app.listen(PORT, () =>
