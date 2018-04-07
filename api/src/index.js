c3f46ca734944886366bbe85ef9b18f7c5040239
Ivan Skorokhodov <iskorokhodov@gmail.com>
2018-04-07 19:41:56 +0300

    fix: add CORS to everything

--- api/src/index.js
+++ api/src/index.js
@@ -8,7 +8,7 @@ let app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.all('/', function(req, res, next) {
+app.all('*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With');
 
