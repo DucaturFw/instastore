e7e30788b6e9309493985b592ea138e93fcd2cf8
Ivan Skorokhodov <iskorokhodov@gmail.com>
2018-04-07 19:14:45 +0300

    feat: allow CORS

--- api/src/index.js
+++ api/src/index.js
@@ -1,12 +1,19 @@
 let express = require('express');
-var bodyParser = require("body-parser");
+var bodyParser = require('body-parser');
 
-var routes = require("./routes.js");
+var routes = require('./routes.js');
 
 let app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.all('/', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+
+    next();
+});
 
 routes(app);
 
